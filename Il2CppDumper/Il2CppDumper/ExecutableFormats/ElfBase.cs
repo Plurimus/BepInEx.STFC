a918c8addbc4176f32d8131b765be97e005c7300
Optimus <asd>
2025-02-24 13:42:20 +0300

    add Il2CppDumper Prefare

---
+++ Il2CppDumper/Il2CppDumper/ExecutableFormats/ElfBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Il2CppDumper
+{
+    public abstract class ElfBase : Il2Cpp
+    {
+        public bool IsDumped;
+        public ulong DumpAddr;
+
+        protected ElfBase(Stream stream, Action<string> reportProgressAction) : base(stream, reportProgressAction) { }
+
+        public void GetDumpAddress()
+        {
+            reportProgressAction("Detected this may be a dump file.");
+            reportProgressAction("Forcing continuation...");
+        }
+    }
+}
---
+++ Il2CppDumper/Il2CppDumper/Outputs/DummyAssemblyExporter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Il2CppDumper
+{
+    public static class DummyAssemblyExporter
+    {
+        public static void Export(Il2CppExecutor il2CppExecutor, string outputDir, bool addToken)
+        {
+	        string dummyDllPath = Path.Combine(outputDir, "DummyDll");
+
+            if (Directory.Exists(dummyDllPath))
+                Directory.Delete(dummyDllPath, true);
+            Directory.CreateDirectory(dummyDllPath);
+
+            var dummy = new DummyAssemblyGenerator(il2CppExecutor, addToken);
+            foreach (var assembly in dummy.Assemblies)
+            {
+	            string path = Path.Combine(dummyDllPath, assembly.MainModule.Name);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    assembly.Write(stream);
+                    assembly.Dispose();
+                }
+            }
+        }
+    }
+}
